d0416e570377388d8a5dbc08bdb4d18a1c45ce60
shahina <shahinashaik2504@gmail.com>
2019-05-24 13:07:29 +0530

    tested autowire byType

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -6,11 +6,30 @@ public class Movie {
     private String movieName;
     private Actor actor;
 
-    public Movie(int movieId, String movieName, Actor actor) {
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
         this.movieId = movieId;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
         this.movieName = movieName;
+    }
+
+    public Actor getActor() {
+        return actor;
+    }
+
+    public void setActor(Actor actor) {
         this.actor = actor;
     }
+
     public Movie(){
 
     }
