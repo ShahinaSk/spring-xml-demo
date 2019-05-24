4e5c6cd254c774188c40ab3b6e00a88e19c1400c
shahina <shahinashaik2504@gmail.com>
2019-05-24 12:27:51 +0530

    tested scope of bean with prototype scope using ApplicationContext

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,44 +1,21 @@
 package com.stackroute;
 
 import com.stackroute.domain.Movie;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.support.BeanDefinitionReader;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
 
 public class Main
 {
     @Deprecated
     public static void main( String[] args ) {
 
-        //using BeanFactory
-        ClassPathResource resource= new ClassPathResource("beans.xml");
-        BeanFactory beanFactory=new XmlBeanFactory(resource);
-        Movie movie1=beanFactory.getBean("movie",Movie.class);
-        System.out.println("Actor details using BeanFactory:\n"+movie1+"\n");
-
         //using ApplicationContext
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie2=(Movie)applicationContext.getBean("movie");
-        System.out.println("Actor details using ApplicationContext:\n"+movie2+"\n");
+        Movie movieA=(Movie)applicationContext.getBean("movieA");
+        Movie movieB=(Movie)applicationContext.getBean("movieB");
 
-        //using beanDefinitionRegistry
-        BeanDefinitionRegistry beanDefinitionRegistry = new DefaultListableBeanFactory();
-        BeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanDefinitionRegistry);
-        beanDefinitionReader.loadBeanDefinitions(new ClassPathResource("beans.xml"));
-        Movie movie3=(Movie) ((DefaultListableBeanFactory) beanDefinitionRegistry).getBean("movie");
-        System.out.println("Actor details using BeanDefinitionRegistry:\n"+movie3);
+        System.out.println(movieA==movieB);
 
-        //testing scope of ApplicationContext
-        ApplicationContext applicationContext2 = new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie4=(Movie)applicationContext2.getBean("movie");
-        Movie movie5=(Movie)applicationContext2.getBean("movie");
 
-        System.out.println(movie5==movie4);
     }
 }
